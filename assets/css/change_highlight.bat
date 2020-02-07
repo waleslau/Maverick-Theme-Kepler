89c4702370752110f0d40be92ae1c15df540809a
waleslau <waleslau@foxmail.com>
2020-02-07 19:50:03 +0800

    change_highlight

---
+++ assets/css/change_highlight.bat
@@ -0,0 +1,8 @@
+:: change_highlight
+
+git pull
+git add .
+git commit -m "change_highlight"
+git push
+
+pause
---
+++ assets/css/change_highlight.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+git pull
+git add .
+git commit -m "change highlight"
+git push
